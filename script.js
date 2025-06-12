5277a5c4776c8a8f42a836fce6db562b385a34e5
rissastar <larissastar0316@gmail.com>
2025-06-12 00:46:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// === Dropdown Navigation ===
+document.getElementById("dropdownToggle").addEventListener("click", () => {
+  const dropdown = document.getElementById("topicDropdown");
+  dropdown.classList.toggle("hidden");
+});
